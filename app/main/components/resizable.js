bb7d7c1d80c2020cc4e585b9b96735aeee9f1118
Artur.K <sanderson180610@gmail.com>
2017-04-04 00:09:23 +0300

    resize base

--- app/main/components/resizable.js
+++ app/main/components/resizable.js
@@ -4,8 +4,40 @@ angular
         controller: ResizableController
     });
 
-  function ResizableController() {
+  function ResizableController($element) {
       var $ctrl = this;
 
+      $ctrl.$postLink = init;
 
+      function init() {
+          $element.on('mouseenter', function (e) {
+              showControls();
+          });
+
+          $element.on('mouseleave', function (e) {
+              removeControls();
+          });
+      }
+
+
+      function showControls() {
+          var overlayData = $element[0].getBoundingClientRect();
+          var width = 50;
+          var height = 50;
+          var overlayElement = angular.element('<div class="resize-ctrl" id="resize-corner"></div>').css({
+              'background-color': '#000',
+              position: 'absolute',
+              left: overlayData.width - width + 'px',
+              top: overlayData.height - height + 'px',
+              width: width + 'px',
+              height: height + 'px'
+          });
+
+          $element.append(overlayElement);
+      }
+
+      function removeControls() {
+          var resizeElements = document.getElementsByClassName('resize-ctrl');
+          angular.element(resizeElements).remove();
+      }
   }
